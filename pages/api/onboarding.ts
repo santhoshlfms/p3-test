f6b5775b34622b1f32393f542579d385fe4d95e8
santhoshlfms <santhoshlfms@gmail.com>
2023-02-23 14:39:49 +0530

    adding test page

--- pages/api/onboarding.ts
+++ pages/api/onboarding.ts
@@ -6,100 +6,103 @@ type Data = {
 };
 
 var consumerOnboardingData = JSON.stringify({
-  "person_details": {
-    "names": [
+  person_details: {
+    names: [
       {
-        "given_name": "hellow",
-        "surname": "raj",
-        "confirmation": {
-          "status": "CONFIRMED"
-        }
-      }
+        given_name: "hellow",
+        surname: "raj",
+        confirmation: {
+          status: "CONFIRMED",
+        },
+      },
     ],
-    "date_of_birth": {
-      "date": "1970-01-01",
-      "confirmation": {
-        "status": "CONFIRMED"
-      }
+    date_of_birth: {
+      date: "1970-01-01",
+      confirmation: {
+        status: "CONFIRMED",
+      },
     },
-    "origin_country_code": "KE",
-    "phone_contacts": [
+    origin_country_code: "KE",
+    phone_contacts: [
       {
-        "phone": {
-          "country_code": "81",
-          "national_number": "8041411111"
+        phone: {
+          country_code: "81",
+          national_number: "8041411111",
         },
-        "phone_type": "MOBILE",
-        "confirmation": {
-          "status": "CONFIRMED"
-        }
-      }
-    ],
-    "addresses": [
-      {
-        "address": {
-          "line1": "58371-00200 City Square",
-          "line2": "Box 2512-00100",
-          "city": "Nairobi",
-          "state": "Nairobi",
-          "country_code": "KE",
-          "postal_code": "20115"
+        phone_type: "MOBILE",
+        confirmation: {
+          status: "CONFIRMED",
         },
-        "address_type": "HOME",
-        "confirmation": {
-          "status": "CONFIRMED"
-        }
-      }
+      },
     ],
-    "email_addresses": [
+    addresses: [
       {
-        "email_address": "app2apptest@pp.com",
-        "primary": true,
-        "confirmed": true
-      }
+        address: {
+          line1: "58371-00200 City Square",
+          line2: "Box 2512-00100",
+          city: "Nairobi",
+          state: "Nairobi",
+          country_code: "KE",
+          postal_code: "20115",
+        },
+        address_type: "HOME",
+        confirmation: {
+          status: "CONFIRMED",
+        },
+      },
     ],
-    "locale": "ke_KE",
-    "nationality": "KE"
+    email_addresses: [
+      {
+        email_address: "app2apptest@pp.com",
+        primary: true,
+        confirmed: true,
+      },
+    ],
+    locale: "ke_KE",
+    nationality: "KE",
   },
-  "paypal_account_properties": {
-    "account_country_code": "KE"
-  }
+  paypal_account_properties: {
+    account_country_code: "KE",
+  },
 });
 
 var linkedInstrumentApiPayload = {
-  "card_accounts": [
+  card_accounts: [
     {
-      "identifier": "4032030646243999",
-      "reference_financial_instrument_id": "B2121XYZ1" +  Date.now().toString(),
-      "expiry_date": "2026-07",
-      "billing_address": {
-        "address_line_1": "58371-00200 City Square",
-        "address_line_2": "Box 2512-00100",
-        "admin_area_1": "Nairobi",
-        "admin_area_2": "Nairobi",
-        "country_code": "KE",
-        "postal_code": "20115"
+      identifier: "4032030646243999",
+      reference_financial_instrument_id: "B2121XYZ1" + Date.now().toString(),
+      expiry_date: "2026-07",
+      billing_address: {
+        address_line_1: "58371-00200 City Square",
+        address_line_2: "Box 2512-00100",
+        admin_area_1: "Nairobi",
+        admin_area_2: "Nairobi",
+        country_code: "KE",
+        postal_code: "20115",
       },
-      "confirmation": {
-        "status": "CONFIRMED"
+      confirmation: {
+        status: "CONFIRMED",
       },
-      "account_holder_name": {
-        "given_name": "Mr tum",
-        "surname": "yum"
-      }
-    }
+      account_holder_name: {
+        given_name: "Mr tum",
+        surname: "yum",
+      },
+    },
   ],
-  "referral_id": ""
-}
+  referral_id: "",
+};
 
 var myHeaders = new Headers();
 myHeaders.append("Content-Type", "application/json");
-myHeaders.append("Authorization", "Basic QVZoM2FxUUl6N1ZaaUd6ZFpxOTJEaFdiRjd4OWo1WWU5dldYVks5ME9BTUgyaWt4dXZBNlkxVUtramI4V3hWUFZKQVdscjdhQk9BTm05VmE6RVBKSzB4TEVvUmJaU1JaY0VqOXVNbWJNWE90TWdDVksxTGRUbDZRSWtaUUNHaG5OWTlqbFRaOE8tbkgyWmRuWU16cXF5UzQ1RXpyTkRaZDk=");
+myHeaders.append(
+  "Authorization",
+  "Basic QVZoM2FxUUl6N1ZaaUd6ZFpxOTJEaFdiRjd4OWo1WWU5dldYVks5ME9BTUgyaWt4dXZBNlkxVUtramI4V3hWUFZKQVdscjdhQk9BTm05VmE6RVBKSzB4TEVvUmJaU1JaY0VqOXVNbWJNWE90TWdDVksxTGRUbDZRSWtaUUNHaG5OWTlqbFRaOE8tbkgyWmRuWU16cXF5UzQ1RXpyTkRaZDk="
+);
 
 var requestOptions = {
-  method: 'POST',
+  method: "POST",
   headers: myHeaders,
-  body: consumerOnboardingData
+  body: consumerOnboardingData,
 };
 
 let config = {
@@ -111,62 +114,70 @@ let config = {
     "Basic QVZoM2FxUUl6N1ZaaUd6ZFpxOTJEaFdiRjd4OWo1WWU5dldYVks5ME9BTUgyaWt4dXZBNlkxVUtramI4V3hWUFZKQVdscjdhQk9BTm05VmE6RVBKSzB4TEVvUmJaU1JaY0VqOXVNbWJNWE90TWdDVksxTGRUbDZRSWtaUUNHaG5OWTlqbFRaOE8tbkgyWmRuWU16cXF5UzQ1RXpyTkRaZDk=",
 };
 
-const consumerReferralApi = (requestOptions:any, cb:any) => {
+const consumerReferralApi = (requestOptions: any, cb: any) => {
   requestOptions = {
-    method: 'POST',
+    method: "POST",
     headers: myHeaders,
-    body: consumerOnboardingData
+    body: consumerOnboardingData,
   };
-  fetch("https://api.sandbox.paypal.com/v1/customer/consumer-referrals", requestOptions).then((res) => res.json())
-  .then(
-    (data) => {
-      //cb(data)
-      console.log("completed consumer refferal")
-      console.log(data )
-      linkedInstrumentApi(data, function(resp: any){
-        cb(data)
-      })
-    },
-    (error) => {
-      console.log(error);
-    }
-  );
+  fetch(
+    "https://api.sandbox.paypal.com/v1/customer/consumer-referrals",
+    requestOptions
+  )
+    .then((res) => res.json())
+    .then(
+      (data) => {
+        //cb(data)
+        console.log("completed consumer refferal");
+        console.log(data);
+        linkedInstrumentApi(data, function (resp: any) {
+          cb(data);
+        });
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
 };
-  
 
-
-const linkedInstrumentApi = (data: any, cb:any ) => {
-
-//res.status(200).json({ name: "John Doe" });
-console.log("linkedInstrumentApi", data)
-linkedInstrumentApiPayload.referral_id = data.referral_id;
-console.log(linkedInstrumentApiPayload )
-requestOptions = {
-  method: 'POST',
-  headers: myHeaders,
-  body: JSON.stringify(linkedInstrumentApiPayload)
-};
-//console.log(requestOptions.body )
-fetch("https://api.sandbox.paypal.com/v1/payment-networks/linked-instruments", requestOptions).then((res) => res.json())
-.then(
-  (data) => {
-    console.log(data)
-    cb(data)
-  },
-  (error) => {
-    console.log(error);
-  }
-);
+const linkedInstrumentApi = (data: any, cb: any) => {
+  //res.status(200).json({ name: "John Doe" });
+  console.log("linkedInstrumentApi", data);
+  linkedInstrumentApiPayload.referral_id = data.referral_id;
+  console.log(linkedInstrumentApiPayload);
+  requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: JSON.stringify(linkedInstrumentApiPayload),
+  };
+  //console.log(requestOptions.body )
+  fetch(
+    "https://api.sandbox.paypal.com/v1/payment-networks/linked-instruments",
+    requestOptions
+  )
+    .then((res) => res.json())
+    .then(
+      (data) => {
+        console.log(data);
+        cb(data);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
 };
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  consumerReferralApi(requestOptions, function(data:any){
-    console.log(data)
-    let onboardingURL = `https://www.sandbox.paypal.com/consumeronboarding/entry?referralid=`+data.referral_id+`&redirect_uri=https://shopping-paypal.herokuapp.com/thankyou&state=channel%3DMobile%26Linkid%3Dtest&scope=scope_group_provisioning_platform`
-    console.log(onboardingURL)
+  consumerReferralApi(requestOptions, function (data: any) {
+    console.log(data);
+    let onboardingURL =
+      `https://www.sandbox.paypal.com/consumeronboarding/entry?referralid=` +
+      data.referral_id +
+      `&redirect_uri=https://p3-test.vercel.app/test&state=channel%3DMobile%26Linkid%3Dtest&scope=scope_group_provisioning_platform`;
+    console.log(onboardingURL);
     res.status(200).json({ url: onboardingURL });
-  })
+  });
 }
--- pages/test.tsx
+++ pages/test.tsx
@@ -1,34 +1,22 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
 import React from "react";
-import { Container, Row, Col, Navbar, Button } from "react-bootstrap";
 
 const Home: NextPage = () => {
   const Onboarding = () => {
     window.location = "https://shopping-paypal.herokuapp.com/thankyou";
+    //window.location = "https://p3-test.vercel.app/test";
   };
 
   return (
     <div>
-      <Navbar bg="light">
-        <Container>
-          <Navbar.Brand href="#home">
-            PayPal Provisioning Platform{" "}
-          </Navbar.Brand>
-        </Container>
-      </Navbar>
-
-      <Container className="mt-3">
-        <Row>
+      <div className="mt-3">
+        <div>
           PayPal linking flow complete - Success !!
-          <Col>
-            <Button variant="primary" onClick={() => Onboarding()}>
-              Continue to BISB APP{" "}
-            </Button>{" "}
-          </Col>
-        </Row>
-      </Container>
+          <div>
+            <button onClick={() => Onboarding()}>Continue to BISB APP </button>{" "}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
